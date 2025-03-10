54c86fd948b50c9ac93b80cafaa16c774363a103
Banzay0184 <shakhzodabidov@gmail.com>
2025-03-11 02:09:05 +0500

    0.2

--- frontend/src/components/TruckJourney.jsx
+++ frontend/src/components/TruckJourney.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import trackimg from "../assets/trackimg.png";
+import trackimg from "../assets/trackImg.png";
 
 const TruckJourney = () => {
     // Варианты анимации для грузовика
