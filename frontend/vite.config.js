6f65f488e3036f2ea5940c51a1114b5d7afad74a
Macbook <macbook@192.168.0.185>
2025-04-28 00:35:29 +0500

    34

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), react()],
+    plugins: [react(), tailwindcss()],
     server: {
         port: 3000,
         proxy: {
-            '/api/v1': {
-                target: 'https://optima.fly.dev',
+            '/api': {
+                target: 'https://optima.fly.dev/api/v1',
                 changeOrigin: true,
-                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
     }
