7f4b4efd1b551a4b96fc4ab66eced7644b3548a2
Macbook <macbook@192.168.0.185>
2025-04-27 23:46:45 +0500

    26

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/code/data/db.sqlite3'
+        'NAME': BASE_DIR / '/code/data/db.sqlite3'
     }
 }
 
